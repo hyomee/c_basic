af25c331c489bb4eaa3a7b5a68461b8830305c9b
hyosang <hyomee@naver.com>
2022-01-08 23:11:45 +0900

    .

---
+++ src/codeup/02_intout/1016.c
@@ -0,0 +1,20 @@
+/**
+ * @file 1016.c
+ * @author your name (you@domain.com)
+ * @brief 정수 한 개 입력받아 세 번 출력하기
+    입력 : 정수 하나가 입력
+    출력 : 입력받은 정수를 공백으로 구분해 세 번 출력 
+ * @version 0.1
+ * @date 2022-01-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+int main (void) { 
+    int a; 
+    scanf("%d", &a); 
+    printf("%d %d %d", a, a, a); 
+    return 0; 
+}
+
