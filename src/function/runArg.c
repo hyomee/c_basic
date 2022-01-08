fd7f7cd90873304bc1dcf0a4a4df83fd66a8b8fd
hyosang <hyomee@naver.com>
2022-01-08 20:59:24 +0900

    runArc

---
+++ src/function/runArg.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+ 
+void main(char argc, char *argv[]) { 
+	printf("argc :  %d\n", argc); 		        // argc  실행인자의 갯수	
+	printf("argc[1] :  %s\n", argv[1]); 		// argv 파라메터 
+	printf("argc[2] :  %s\n", argv[2]);
+}
