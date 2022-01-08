08f63d5789cb92b23a865d8bb75d2702b489e8eb
hyosang <hyomee@naver.com>
2022-01-08 23:14:51 +0900

    추가

---
+++ src/codeup/01_out/1009.c
@@ -0,0 +1,19 @@
+/**
+ * @file 1009.c
+ * @author your name (you@domain.com)
+ * @brief 정수 한 개 입력받아 그대로 출력하기
+    입력 : 정수값 한 개가 입력
+    출력 : 입력된 정수를 그대로 출력
+ * @version 0.1
+ * @date 2022-01-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+int main (void) { 
+    int a; 
+    scanf("%d", &a); 
+    printf("%d", a); 
+    return 0; 
+}
