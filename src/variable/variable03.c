55c94700630b450399fa13426508b6db81b223ac
hyosang <hyomee@naver.com>
2022-01-08 20:43:27 +0900

    변수

---
+++ src/variable/variable03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * 요구사항 
+ * 1. 변수 여러개를 선언 하고 각각 초기화 한다.
+ */
+int main() {
+    int age, price, count; 	    // 변수 여러개를 한번에 선언
+   
+    age = 30;
+    price = 1000;
+    count = 1;
+
+    printf("%d, %d, %d", age, price, count); 
+
+}
---
+++ src/variable/variable04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * 요구사항 
+ * 1. 변수 여러개를 한번애 선언, 초기화 
+ */
+int main() {
+    int age=30, price, count; 	 
+
+	price = 1000;
+	count = 1;
+
+
+	printf("%d, %d, %d", age, price, count); 
+    return 0;
+}
