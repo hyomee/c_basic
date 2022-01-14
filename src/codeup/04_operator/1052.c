aca90951a3ee3bfbbe5f9997a061421a7a54b598
hyosang <hyomee@naver.com>
2022-01-15 02:05:36 +0900

    ....

---
+++ src/codeup/04_operator/1052.c
@@ -0,0 +1,22 @@
+/**
+ * @file 1052.c
+ * @author your name (you@domain.com)
+ * @brief 두 정수 입력받아 비교하기4
+   두 정수(a, b)를 입력받아 a와 b가 서로 다르면 1을, 그렇지 않으면 0을 출력하는 프로그램을 작성해보자\\
+   입력 : 두 정수 a, b가 공백을 두고 입력된다. -2147483647 <= a, b <= +2147483648
+   출력 : a와 b가 다른 경우 1을, 그렇지 않은 경우 0을 출력한다.
+   -- 
+ * @version 0.1
+ * @date 2022-01-15
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a!=b);
+	return 0;
+}
---
+++ src/codeup/readme.md
@@ -0,0 +1,3 @@
+CodeUp 2.0 에서 c기초 문제 100 풀이
+
+https://codeup.kr/problemsetsol.php?psid=23
